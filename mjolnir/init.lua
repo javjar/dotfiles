07054a7780df34a0aeef057987d03a6266fac2dc
Javier Jaramillo <jjaramillo@riotgames.com>
2014-12-24 01:34:07 -0800

    incremental stuff to mjolnir config

--- mjolnir/init.lua
+++ mjolnir/init.lua
@@ -3,13 +3,19 @@ local hotkey = require "mjolnir.hotkey"
 local window = require "mjolnir.window"
 local fnutils = require "mjolnir.fnutils"
 local alert = require "mjolnir.alert"
+local grid = require "mjolnir.bg.grid"
+-- Music controls
+local spotify = require "mjolnir.lb.spotify"
 
-local mash = {"ctrl", "alt", "cmd"}
+-- Set up hotkey combinations
+local mash = {"cmd", "alt", "ctrl"}
+local mashshift = {"cmd", "alt", "shift"}
 
-hotkey.bind(mash, "m", function()
-  local win = window.focusedwindow()
-  win:maximize()
-end)
+-- Set grid size.
+grid.GRIDWIDTH  = 6
+grid.GRIDHEIGHT = 4
+grid.MARGINX = 0
+grid.MARGINY = 0
 
 local function shiftfourth(x, y)
   local win = window.focusedwindow()
@@ -43,6 +49,17 @@ local function shifthalf(x, y, w, h)
   win:setframe(newframe)
 end
 
+hotkey.bind(mash, "m", function()
+  local win = window.focusedwindow()
+  win:maximize()
+end)
+
+hotkey.bind(mash, ';', function() grid.snap(window.focusedwindow()) end)
+hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), grid.snap) end)
+
+hotkey.bind(mash, '=', function() grid.adjustwidth( 1) end)
+hotkey.bind(mash, '-', function() grid.adjustwidth(-1) end)
+
 hotkey.bind(mash, "1", function() shiftfourth(0, 0) end)
 hotkey.bind(mash, "2", function() shiftfourth(1, 0) end)
 hotkey.bind(mash, "3", function() shiftfourth(0, 1) end)
@@ -53,8 +70,28 @@ hotkey.bind(mash, "right", function() shifthalf(1, 0, 2, 1) end)
 hotkey.bind(mash, "up", function() shifthalf(0, 0, 1, 2) end)
 hotkey.bind(mash, "down", function() shifthalf(0, 1, 1, 2) end)
 
+hotkey.bind(mash, 'N', grid.pushwindow_nextscreen)
+hotkey.bind(mash, 'P', grid.pushwindow_prevscreen)
+
+hotkey.bind(mash, 'J', grid.pushwindow_down)
+hotkey.bind(mash, 'K', grid.pushwindow_up)
+hotkey.bind(mash, 'H', grid.pushwindow_left)
+hotkey.bind(mash, 'L', grid.pushwindow_right)
+
+hotkey.bind(mash, 'U', grid.resizewindow_taller)
+hotkey.bind(mash, 'O', grid.resizewindow_wider)
+hotkey.bind(mash, 'I', grid.resizewindow_thinner)
+hotkey.bind(mash, 'Y', grid.resizewindow_shorter)
+
+hotkey.bind(mashshift, 'space', spotify.displayCurrentTrack)
+hotkey.bind(mashshift, 'p', spotify.play)
+hotkey.bind(mashshift, 'o', spotify.pause)
+hotkey.bind(mashshift, 'l', spotify.next)
+hotkey.bind(mashshift, 'k', spotify.previous)
+
 alert.show('ShiftIt\'ed')
 
 -- TODO:
 -- center current window at current size or ~80%
--- increase/decrease size by ~10%
+-- increase/decrease size by ~10% with -/+
+-- fix the window's titlebar not being taken into consideration
