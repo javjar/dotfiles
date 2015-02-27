7b0c81259686b6ba42cbfc94ef6d70f32f9669bb
Javier Jaramillo <jjaramillo@riotgames.com>
2015-02-27 14:22:29 -0800

    Fixed some bugs, 1,2,3,4 and arrow keys work as expected. Plus margin error fixed.

--- mjolnir/init.lua
+++ mjolnir/init.lua
@@ -4,6 +4,7 @@ local window = require "mjolnir.window"
 local fnutils = require "mjolnir.fnutils"
 local alert = require "mjolnir.alert"
 local grid = require "mjolnir.bg.grid"
+
 -- Music controls
 local spotify = require "mjolnir.lb.spotify"
 
@@ -12,53 +13,60 @@ local mash = {"cmd", "alt", "ctrl"}
 local mashshift = {"cmd", "alt", "shift"}
 
 -- Set grid size.
-grid.GRIDWIDTH  = 6
-grid.GRIDHEIGHT = 4
+grid.GRIDWIDTH  = 8
+grid.GRIDHEIGHT = 8
 grid.MARGINX = 0
 grid.MARGINY = 0
 
 local function shiftfourth(x, y)
-  local win = window.focusedwindow()
-  if not win then return end -- If no focused window, just return
+  fun = function(f)
+    f.w = grid.GRIDWIDTH/2
+    f.h = grid.GRIDHEIGHT/2
+    f.x = grid.GRIDWIDTH/2 * x
+    f.y = grid.GRIDHEIGHT/2 * y
+  end
+  grid.adjust_focused_window(fun)
 
-  local winframe = win:frame()
-  local screenframe = win:screen():frame()
-  local newframe = {
-    x = screenframe.w / 2 * x,
-    y = screenframe.h / 2 * y,
-    w = screenframe.w / 2,
-    h = screenframe.h / 2
-  }
-
-  win:setframe(newframe)
+  -- print("Shiftfourth")
+  -- print("x: ", x, ", y: ", y)
 end
 
 local function shifthalf(x, y, w, h)
-  local win = window.focusedwindow()
-  if not win then return end -- If no focused window, just return
+  fun = function(f)
+    f.w = grid.GRIDWIDTH / w
+    f.h = grid.GRIDHEIGHT / h
+    f.x = grid.GRIDWIDTH / 2 * x
+    f.y = grid.GRIDHEIGHT / 2 * y
+  end
+  grid.adjust_focused_window(fun)
 
-  local winframe = win:frame()
-  local screenframe = win:screen():frame()
-  local newframe = {
-    x = screenframe.w / 2 * x,
-    y = screenframe.h / 2 * y,
-    w = screenframe.w / w,
-    h = screenframe.h / h
-  }
-
-  win:setframe(newframe)
+  -- print("Shifthalf")
+  -- print("x: ", x, ", y: ", y, ", w: ", w, ", h: ", h)
 end
 
-hotkey.bind(mash, "m", function()
+local function shiftcenter()
+  fun = function(f)
+    f.w = grid.GRIDWIDTH - 1
+    f.h = grid.GRIDHEIGHT - 1
+    f.x = 1
+    f.y = 1
+  end
+  grid.adjust_focused_window(fun)
+
+  -- print("Shifcenter")
+  -- print("x: ", x, ", y: ", y, ", w: ", w, ", h: ", h)
+end
+
+local function shiftfullscreen()
   local win = window.focusedwindow()
   win:maximize()
-end)
+end
 
 hotkey.bind(mash, ';', function() grid.snap(window.focusedwindow()) end)
 hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), grid.snap) end)
 
-hotkey.bind(mash, '=', function() grid.adjustwidth( 1) end)
-hotkey.bind(mash, '-', function() grid.adjustwidth(-1) end)
+-- hotkey.bind(mash, '=', function() grid.adjustwidth( 1) end)
+-- hotkey.bind(mash, '-', function() grid.adjustwidth(-1) end)
 
 hotkey.bind(mash, "1", function() shiftfourth(0, 0) end)
 hotkey.bind(mash, "2", function() shiftfourth(1, 0) end)
@@ -69,6 +77,9 @@ hotkey.bind(mash, "left", function() shifthalf(0, 0, 2, 1) end)
 hotkey.bind(mash, "right", function() shifthalf(1, 0, 2, 1) end)
 hotkey.bind(mash, "up", function() shifthalf(0, 0, 1, 2) end)
 hotkey.bind(mash, "down", function() shifthalf(0, 1, 1, 2) end)
+
+hotkey.bind(mash, 'm', function() shiftfullscreen() end)
+hotkey.bind(mash, 'c', function() shiftcenter() end)
 
 hotkey.bind(mash, 'N', grid.pushwindow_nextscreen)
 hotkey.bind(mash, 'P', grid.pushwindow_prevscreen)
@@ -90,8 +101,3 @@ hotkey.bind(mashshift, 'l', spotify.next)
 hotkey.bind(mashshift, 'k', spotify.previous)
 
 alert.show('ShiftIt\'ed')
-
--- TODO:
--- center current window at current size or ~80%
--- increase/decrease size by ~10% with -/+
--- fix the window's titlebar not being taken into consideration
